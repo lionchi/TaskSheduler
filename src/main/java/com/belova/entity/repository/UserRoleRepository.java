46922c62809a19e45f091fdd9159fc8c573b086b
TanyaBelova <tanya.belova19@yandex.ru>
2020-01-04 18:41:03 +0300

    test add

--- src/main/java/com/belova/entity/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.belova.entity.repository;
-
-import com.belova.entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long>, JpaSpecificationExecutor<UserRole> {
-    UserRole findByRolename (String name);
-}
